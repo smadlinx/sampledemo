14d06a504487a3708a4d475c90c140be46e00503
smadlinx <105612187+smadlinx@users.noreply.github.com>
2025-05-14 15:10:06 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+function openNav() {
+      document.getElementById("mySidenav").style.width = "250px";
+      document.getElementById("main").style.marginRight = "250px";
+      document.getElementById("openBtn").style.right = "270px";
+    }
+    
+    function closeNav() {
+      document.getElementById("mySidenav").style.width = "0";
+      document.getElementById("main").style.marginRight = "0";
+      document.getElementById("openBtn").style.right = "20px";
+    }
